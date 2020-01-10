01f404654045271d0a1970d2d3cb06d4fc7a54bd
saunak <saunakstha@gmail.com>
2020-01-10 12:23:02 +0545

    Hey man

---
+++ src.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    printf("Coding Message");
+    char c,message;
+    printf("Enter the message");
+    scanf("%c",&message);
+    printf("Your message is %c",message);
+    return 0;
+}
