c86d1150f3319ef0189fddbc1251a6224a97b872
saunak <saunakstha@gmail.com>
2020-01-10 13:14:46 +0545

    first change

--- src.c
+++ src.c
@@ -2,9 +2,5 @@
 #include<string.h>
 int main(){
     printf("Coding Message");
-    char c,message;
-    printf("Enter the message");
-    scanf("%c",&message);
-    printf("Your message is %c",message);
     return 0;
 }
